baaa12f92816f42a967c9bc8e5749b2c3493aa92
Jack Lambert <jacklmbrt07@gmail.com>
2022-10-07 11:13:45 -0700

    Not sure why expense form isnt working

--- src/Components/ExpenseRow.js
+++ src/Components/ExpenseRow.js
@@ -1,7 +1,9 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { nanoid } from "nanoid";
 
 const ExpenseRow = ({ expensesData, name, key }) => {
+  const categoryNames = ["Food", "Travel", "Equipment"];
+
   const [expenses, setExpenses] = useState(expensesData);
 
   const [addExpenseFormData, setAddExpenseFormData] = useState({
@@ -10,6 +12,7 @@ const ExpenseRow = ({ expensesData, name, key }) => {
     cost: 0,
   });
 
+  // how can I pass in these functions in without iterating form?
   const handleAddExpenseFormChange = (event) => {
     event.preventDefault();
 
@@ -29,14 +32,13 @@ const ExpenseRow = ({ expensesData, name, key }) => {
       id: nanoid(),
       category: addExpenseFormData.category,
       description: addExpenseFormData.description,
-      cost: addExpenseFormData.cost
+      cost: addExpenseFormData.cost,
     };
-    
+
     const newExpenses = [...expenses, newExpense];
 
     setExpenses(newExpenses);
   };
-
 
   console.log("contact.expenses", expenses);
   return (
@@ -51,9 +53,44 @@ const ExpenseRow = ({ expensesData, name, key }) => {
           </tr>
         );
       })}
+      <form onSubmit={handleAddExpenseFormSubmit}>
+        <h2>Add an Expense</h2>
+        <label for="expenses">Choose a person:</label>
+        <select id="fullName">
+          <option value={name}>{name}</option>
+        </select>
+        <label for="category">Choose a category:</label>
+        <select
+          name="expense.category"
+          id="category"
+          onChange={handleAddExpenseFormChange}
+        >
+          {categoryNames.map((name, i) => (
+            <option key={i} value={name}>
+              {name}
+            </option>
+          ))}
+        </select>
+        <input
+          type="text"
+          name="expense.description"
+          required
+          placeholder="Enter Description..."
+          onChange={handleAddExpenseFormChange}
+        />
+        <input
+          type="number"
+          step="0.01"
+          max="2500"
+          name="expense.cost"
+          required
+          placeholder="Enter Cost..."
+          onChange={handleAddExpenseFormChange}
+        />
+        <button type="submit">Add</button>
+      </form>{" "}
     </React.Fragment>
   );
 };
 
 export default ExpenseRow;
-
